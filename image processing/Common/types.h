722174148fbc7fac3f0e36bcd136ca4ef7413be4
Ahmed Maged <mageda9000@gmail.com>
2023-04-07 14:28:33 +0200

    add move/copy constructor and =operators

--- image processing/Common/types.h
+++ image processing/Common/types.h
@@ -119,17 +119,55 @@ namespace qlm
 	public:
 		const unsigned int width;
 		const unsigned int height;
-	
+		// Constructor
 		Kernel(unsigned int M, unsigned int N) :width(M), height(N)
 		{
 			data = new float[height * width];
 		}
+		// Move Constructor
+		Kernel(Kernel&& other) : width(other.width), height(other.height), data(other.data)
+		{
+			other.data = nullptr;
+		}
+		// Copy Constructor
+		Kernel(const Kernel& other) : width(other.width), height(other.height)
+		{
+			data = new float[height * width];
+			std::memcpy(data, other.data, height * width * sizeof(float));
+		}
+		// Move Assignment Operator
+		Kernel& operator=(Kernel&& other) 
+		{
+			if (this != &other)
+			{
+				const_cast<unsigned int&>(width) = other.width;
+				const_cast<unsigned int&>(height) = other.height;
+				data = other.data;
+				other.data = nullptr;
+			}
+			return *this;
+		}
+		// Copy Assignment Operator
+		Kernel& operator=(const Kernel& other)
+		{
+			if (this != &other)
+			{
+				const_cast<unsigned int&>(width) = other.width;
+				const_cast<unsigned int&>(height) = other.height;
+				if (data != nullptr)
+					delete[] data;
+				data = new float[height * width];
+				std::memcpy(data, other.data, height * width * sizeof(float));
+			}
+			return *this;
+		}
+		// Destructor
 		~Kernel()
 		{
 			if (data != nullptr)
 				delete[] data;
 		}
-	
+	public:
 		float Get(int x, int y) const
 		{
 			return data[y * width + x];
@@ -144,10 +182,40 @@ namespace qlm
 	class Kernel1D : protected Kernel
 	{
 	public:
+		const unsigned int length;
+		// Constructor
 		Kernel1D(unsigned int length): Kernel(1u, length), length(length)
 		{}
+		// Copy Constructor
+		Kernel1D(const Kernel1D& other) : Kernel(other), length(other.length)
+		{
+		}
+		// Move Constructor
+		Kernel1D(Kernel1D&& other) : Kernel(std::move(other)), length(other.length)
+		{
+		}
+		// Copy Assignment Operator
+		Kernel1D& operator=(const Kernel1D& other)
+		{
+			if (this != &other)
+			{
+				Kernel::operator=(other);
+				const_cast<unsigned int&>(length) = other.length;
+			}
+			return *this;
+		}
+		// Move Assignment Operator
+		Kernel1D& operator=(Kernel1D&& other) noexcept
+		{
+			if (this != &other)
+			{
+				Kernel::operator=(std::move(other));
+				const_cast<unsigned int&>(length) = other.length;
+			}
+			return *this;
+		}
+
 	public:
-		const unsigned int length;
 
 		float Get(int idx)  const
 		{
