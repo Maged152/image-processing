bea70c1b78d7c31e167231e5e94bd2c64cfd8dbf
Ahmed Maged <mageda9000@gmail.com>
2023-04-14 18:19:23 +0200

    move to qlm namespace

--- image processing/Common/Timer.h
+++ image processing/Common/Timer.h
@@ -3,35 +3,36 @@
 #include <chrono>
 #include <iostream>
 
-using time_point = std::chrono::time_point<std::chrono::steady_clock>;
-using sec = std::chrono::seconds;
-using msec = std::chrono::milliseconds;
-using usec = std::chrono::microseconds;
-using nsec = std::chrono::nanoseconds;
-
-template<typename T>
-const char* GetType()
-{
-	if constexpr (std::is_same<T, sec>::value)
-	{
-		return " sec\n";
-	}
-	else if constexpr (std::is_same<T, msec>::value)
-	{
-		return " msec\n";
-	}
-	else if constexpr (std::is_same<T, usec>::value)
-	{
-		return " usec\n";
-	}
-	else
-	{
-		return " nsec";
-	}	
-}
-
 namespace qlm
 {
+	using time_point = std::chrono::time_point<std::chrono::steady_clock>;
+	using sec = std::chrono::seconds;
+	using msec = std::chrono::milliseconds;
+	using usec = std::chrono::microseconds;
+	using nsec = std::chrono::nanoseconds;
+
+	template<typename T>
+	const char* GetType()
+	{
+		if constexpr (std::is_same<T, sec>::value)
+		{
+			return " sec\n";
+		}
+		else if constexpr (std::is_same<T, msec>::value)
+		{
+			return " msec\n";
+		}
+		else if constexpr (std::is_same<T, usec>::value)
+		{
+			return " usec\n";
+		}
+		else
+		{
+			return " nsec";
+		}	
+	}
+
+
 	template<typename Duration>
 	struct Timer
 	{
