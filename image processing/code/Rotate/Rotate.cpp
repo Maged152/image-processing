cc539cb01359b8a048faa2e51a6cfa78dafde99a
Ahmed Maged <mageda9000@gmail.com>
2023-05-14 22:18:03 +0300

    finish rotate function

--- image processing/code/Rotate/Rotate.cpp
+++ image processing/code/Rotate/Rotate.cpp
@@ -2,6 +2,7 @@
 
 #include "Rotate.h"
 #include <cmath>
+#include <functional>
 
 namespace qlm
 {
@@ -12,65 +13,63 @@ namespace qlm
 		unsigned int img_height = in.Height();
 		// output image
 		Image<frmt, T> out = Image<frmt, T>{};
+		// temp out pixel
+		Pixel<frmt, T> out_pix;
+		// output location
+		int x_n{ 0 }, y_n{ 0 };
+		// rotate 180
+		std::function<void(int, int)> rotate_180_lambda = [img_width, img_height, &in, &out, &out_pix](int x, int y)
+		{
+			out_pix = in.GetPixel(img_width -1 - x, img_height - 1 - y);
+			out.SetPixel(x, y, out_pix);
+		};
+		// rotate 90
+		std::function<void(int, int)> rotate_90_lambda = [img_width, img_height, &in, &out, &out_pix](int x, int y)
+		{
+			out_pix = in.GetPixel(x, y);
+			out.SetPixel(img_width - 1 - y, x, out_pix);
+		};
+		// rotate -90
+		std::function<void(int, int)> rotate_270_lambda = [img_width, img_height, &in, &out, &out_pix](int x, int y)
+		{
+			out_pix = in.GetPixel(x, y);
+			out.SetPixel(y, img_height - 1 - x, out_pix);
+		};
+		// function type
+		std::function<void(int, int)> rotate_fun;
+
 		if (rotate_code == RotateFlag::ROTATE_180)
 		{
 			// 180 degrees
 			out.create(img_width, img_height);
+
+			rotate_fun = rotate_180_lambda;
 		}
 		else
 		{
 			// 90 or 270 degrees, swap width , height
 			out.create(img_height, img_width);
-		}
-		int x_c = (img_width - 1) / 2;
-		int y_c = (img_height - 1)/ 2;
-		// rotate function
-		void (*Rotate_lambda)(int, int, int, int, int&, int&);
-		if (rotate_code == RotateFlag::ROTATE_90_CLOCKWISE)
-		{
-			Rotate_lambda = [](int x, int y, int x_c, int y_c, int& x_n, int& y_n)
-			{
-				x_n = -y + y_c + x_c;
-				y_n = x - x_c + y_c;
 
-			};
-		}
-		else if (rotate_code == RotateFlag::ROTATE_90_COUNTERCLOCKWISE)
-		{
-			Rotate_lambda = [](int x, int y, int x_c, int y_c, int& x_n, int& y_n)
+			if (rotate_code == RotateFlag::ROTATE_90_CLOCKWISE)
 			{
-				x_n = y - y_c + x_c;
-				y_n = -x + x_c + y_c;
-			};
-		}
-		else
-		{
-			Rotate_lambda = [](int x, int y, int x_c, int y_c, int& x_n, int& y_n)
+				rotate_fun = rotate_90_lambda;
+			}
+			else
 			{
-				x_n = -x + x_c + x_c;
-				y_n = -y + y_c + y_c;
-			};
+				rotate_fun = rotate_270_lambda;
+			}
 		}
-		// temp out pixel
-		Pixel<frmt, T> out_pix;
-		// output location
-		int x_n{ 0 }, y_n{ 0 };
+		
 		// loop over the image
 		for (int y = 0; y < img_height; y++)
 		{
 			for (int x = 0; x < img_width; x++)
 			{
-				// rotate
-				Rotate_lambda(x, y, x_c, y_c, x_n, y_n);
-				// check boundaries
-				if (x_n < out.Width() && x_n >= 0 && y_n < out.Height() && y_n >= 0)
-				{
-					out_pix = in.GetPixel(x, y);
-					// store pixel value at (x, y) in (x_n, y_n)
-					out.SetPixel(x_n, y_n, out_pix);
-				}
+				rotate_fun(x, y);
 			}
 		}
+		
+
 		return out;
 	}
 
