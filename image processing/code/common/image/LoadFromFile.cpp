4a8ef5f6d79fe80e1d65a6e0980e5d2d23820a93
Ahmed Maged <mageda9000@gmail.com>
2023-09-06 22:21:35 +0300

    move image function to cpp

---
+++ image processing/code/common/image/LoadFromFile.cpp
@@ -0,0 +1,170 @@
+#include "common/image.h"
+#include "stb_image_write.h"
+#include <fstream>
+
+namespace qlm
+{
+	template<ImageFormat frmt, pixel_t T>
+	bool Image<frmt, T>::SaveToFile(const std::string& file_name, bool alpha, int quality)
+	{
+		// I think stb_write supports only U8 !
+
+		// check if the data is valid
+		if (data == nullptr)
+		{
+			return false;
+		}
+		// capture the return value from the stb_image_write functions, which indicates whether the write operation was successful or not.
+		int stb_status{ 0 };
+		int comp{ 3 };
+		T* img_data{ nullptr };
+		if constexpr (frmt == ImageFormat::GRAY)
+		{
+			comp = 1;
+		}
+		// check if alpha will be included
+		if (alpha)
+		{
+			comp++;
+			//img_data = (T*)data;
+			// create new datat that ignores the alpha channel
+			img_data = new T[width * height * comp];
+			// copy the data 
+			if constexpr (frmt == ImageFormat::GRAY)
+			{
+				for (unsigned int i = 0, idx = 0; i < width * height * comp; i += comp, idx++)
+				{
+					img_data[i] = data[idx].v;
+					img_data[i + 1] = data[idx].a;
+				}
+			}
+			else
+			{
+				for (unsigned int i = 0, idx = 0; i < width * height * comp; i += comp, idx++)
+				{
+					if constexpr (frmt == ImageFormat::RGB)
+					{
+						img_data[i] = data[idx].r;
+						img_data[i + 1] = data[idx].g;
+						img_data[i + 2] = data[idx].b;
+						img_data[i + 3] = data[idx].a;
+					}
+					else if constexpr (frmt == ImageFormat::YCrCb)
+					{
+						img_data[i] = data[idx].y;
+						img_data[i + 1] = data[idx].cr;
+						img_data[i + 2] = data[idx].cb;
+						img_data[i + 3] = data[idx].a;
+					}
+					else if constexpr (frmt == ImageFormat::HSV)
+					{
+						img_data[i] = data[idx].h;
+						img_data[i + 1] = data[idx].s;
+						img_data[i + 2] = data[idx].v;
+						img_data[i + 3] = data[idx].a;
+					}
+					else
+					{
+						img_data[i] = data[idx].h;
+						img_data[i + 1] = data[idx].l;
+						img_data[i + 2] = data[idx].s;
+						img_data[i + 3] = data[idx].a;
+					}
+				}
+			}
+		}
+		else
+		{
+			// create new datat that ignores the alpha channel
+			img_data = new T[width * height * comp];
+			// copy the data 
+			if constexpr (frmt == ImageFormat::GRAY)
+			{
+				for (unsigned int i = 0; i < width * height; i++)
+				{
+					img_data[i] = data[i].v;
+				}
+			}
+			else
+			{
+				for (unsigned int i = 0, idx = 0; i < width * height * comp; i += comp, idx++)
+				{
+					if constexpr (frmt == ImageFormat::RGB)
+					{
+						img_data[i] = data[idx].r;
+						img_data[i + 1] = data[idx].g;
+						img_data[i + 2] = data[idx].b;
+					}
+					else if constexpr (frmt == ImageFormat::YCrCb)
+					{
+						img_data[i] = data[idx].y;
+						img_data[i + 1] = data[idx].cr;
+						img_data[i + 2] = data[idx].cb;
+					}
+					else if constexpr (frmt == ImageFormat::HSV)
+					{
+						img_data[i] = data[idx].h;
+						img_data[i + 1] = data[idx].s;
+						img_data[i + 2] = data[idx].v;
+					}
+					else
+					{
+						img_data[i] = data[idx].h;
+						img_data[i + 1] = data[idx].l;
+						img_data[i + 2] = data[idx].s;
+					}
+				}
+
+			}
+		}
+
+		std::string ext = file_name.substr(file_name.find_last_of('.') + 1);
+		std::transform(ext.begin(), ext.end(), ext.begin(), [](unsigned char c) { return std::tolower(c); });
+
+		if (ext == "bmp")
+		{
+			stb_status = stbi_write_bmp(file_name.c_str(), width, height, comp, reinterpret_cast<void*>(img_data));
+		}
+		else if (ext == "pgm")
+		{
+			if (comp == 1)
+			{
+				// Open the output file for writing
+				std::ofstream output_file(file_name, std::ios::binary);
+				if (output_file.is_open())
+				{
+					// Write the PGM header
+					output_file << "P5\n" << width << " " << height << "\n" << std::numeric_limits<T>::max() << "\n";
+
+					// Write the image data
+					output_file.write(reinterpret_cast<char*>(img_data), width * height * sizeof(T));
+
+					// Close the file
+					output_file.close();
+
+					stb_status = 1;
+				}
+			}
+		}
+		else if (ext == "png")
+		{
+			int stride_in_bytes = comp * width * sizeof(T);
+			stb_status = stbi_write_png(file_name.c_str(), width, height, comp, reinterpret_cast<void*>(img_data), stride_in_bytes);
+		}
+		else if (ext == "jpg" || ext == "jpeg")
+		{
+			stb_status = stbi_write_jpg(file_name.c_str(), width, height, comp, reinterpret_cast<void*>(img_data), quality);
+		}
+
+		delete[] img_data;
+
+		img_data = nullptr;
+
+		return stb_status ? true : false;
+	}
+
+	template bool Image<ImageFormat::GRAY, uint8_t>::SaveToFile(const std::string&, bool , int);
+	template bool Image<ImageFormat::RGB, uint8_t>::SaveToFile(const std::string&, bool, int);
+	template bool Image<ImageFormat::GRAY, int16_t>::SaveToFile(const std::string&, bool, int);
+	template bool Image<ImageFormat::RGB, int16_t>::SaveToFile(const std::string&, bool, int);
+}
---
+++ image processing/code/common/image/image.cpp
@@ -0,0 +1,129 @@
+#include "common/image.h"
+#include <algorithm>
+
+namespace qlm
+{
+	// create image
+	template<ImageFormat frmt, pixel_t T>
+	void Image<frmt, T>::create(unsigned int img_width, unsigned int img_height, Pixel<frmt, T> pix)
+	{
+		width = img_width;
+		height = img_height;
+		if constexpr (frmt == ImageFormat::GRAY)
+		{
+			num_of_channels = 2;
+		}
+		else
+		{
+			num_of_channels = 4;
+		}
+		if (data != nullptr)
+			delete[] data;
+		data = new Pixel<frmt, T>[width * height];
+
+		std::fill_n(data, width * height, pix);
+	}
+
+	template<ImageFormat frmt, pixel_t T>
+	void Image<frmt, T>::create(unsigned int img_width, unsigned int img_height)
+	{
+		width = img_width;
+		height = img_height;
+		if constexpr (frmt == ImageFormat::GRAY)
+		{
+			num_of_channels = 2;
+		}
+		else
+		{
+			num_of_channels = 4;
+		}
+		if (data != nullptr)
+			delete[] data;
+		data = new Pixel<frmt, T>[width * height];;
+	}
+
+	// Get pixel
+	template<ImageFormat frmt, pixel_t T>
+	Pixel<frmt, T> Image<frmt, T>::GetPixel(int x, int y, const BorderMode<frmt, T>& border_mode) const
+	{
+		if (x >= 0 && x < width && y >= 0 && y < height)
+		{
+			// not border
+			return this->GetPixel(x, y);
+		}
+		else
+		{
+			// border pixel
+			switch (border_mode.border_type)
+			{
+			case qlm::BorderType::BORDER_CONSTANT:
+			{
+				return border_mode.border_pixel;
+			}
+			case qlm::BorderType::BORDER_REPLICATE:
+			{
+				int x_idx = std::clamp(x, 0, static_cast<int>(width) - 1);
+				int y_idx = std::clamp(y, 0, static_cast<int>(height) - 1);
+				return this->GetPixel(x_idx, y_idx);
+			}
+			case qlm::BorderType::BORDER_REFLECT:
+			{
+				int x_idx = this->ReflectBorderIndex(x, width);
+				int y_idx = this->ReflectBorderIndex(y, height);
+				return this->GetPixel(x_idx, y_idx);
+			}
+			}
+		}
+	}
+
+
+
+
+
+	// explicit instantiation
+	template void Image<ImageFormat::GRAY, uint8_t>::create(unsigned int, unsigned int, Pixel<ImageFormat::GRAY, uint8_t>);
+	template void Image<ImageFormat::GRAY, int16_t>::create(unsigned int, unsigned int, Pixel<ImageFormat::GRAY, int16_t>);
+	template void Image<ImageFormat::GRAY, float>::create(unsigned int, unsigned int, Pixel<ImageFormat::GRAY, float>);
+
+	template void Image<ImageFormat::RGB, uint8_t>::create(unsigned int, unsigned int, Pixel<ImageFormat::RGB, uint8_t>);
+	template void Image<ImageFormat::RGB, int16_t>::create(unsigned int, unsigned int, Pixel<ImageFormat::RGB, int16_t>);
+	template void Image<ImageFormat::RGB, float>::create(unsigned int, unsigned int, Pixel<ImageFormat::RGB, float>);
+
+	template void Image<ImageFormat::HLS, uint8_t>::create(unsigned int, unsigned int, Pixel<ImageFormat::HLS, uint8_t>);
+	template void Image<ImageFormat::HLS, int16_t>::create(unsigned int, unsigned int, Pixel<ImageFormat::HLS, int16_t>);
+	template void Image<ImageFormat::HLS, float>::create(unsigned int, unsigned int, Pixel<ImageFormat::HLS, float>);
+
+	template void Image<ImageFormat::HSV, uint8_t>::create(unsigned int, unsigned int, Pixel<ImageFormat::HSV, uint8_t>);
+	template void Image<ImageFormat::HSV, int16_t>::create(unsigned int, unsigned int, Pixel<ImageFormat::HSV, int16_t>);
+	template void Image<ImageFormat::HSV, float>::create(unsigned int, unsigned int, Pixel<ImageFormat::HSV, float>);
+
+	template void Image<ImageFormat::YCrCb, uint8_t>::create(unsigned int, unsigned int, Pixel<ImageFormat::YCrCb, uint8_t>);
+	template void Image<ImageFormat::YCrCb, int16_t>::create(unsigned int, unsigned int, Pixel<ImageFormat::YCrCb, int16_t>);
+	template void Image<ImageFormat::YCrCb, float>::create(unsigned int, unsigned int, Pixel<ImageFormat::YCrCb, float>);
+
+	// -------------------------------------------------------------------------------------------------------------
+	template void Image<ImageFormat::GRAY, uint8_t>::create(unsigned int, unsigned int);
+	template void Image<ImageFormat::GRAY, int16_t>::create(unsigned int, unsigned int);
+	template void Image<ImageFormat::GRAY, float>::create(unsigned int, unsigned int);
+	template void Image<ImageFormat::RGB, uint8_t>::create(unsigned int, unsigned int);
+	template void Image<ImageFormat::RGB, int16_t>::create(unsigned int, unsigned int);
+	template void Image<ImageFormat::RGB, float>::create(unsigned int, unsigned int);
+	template void Image<ImageFormat::HLS, uint8_t>::create(unsigned int, unsigned int);
+	template void Image<ImageFormat::HLS, int16_t>::create(unsigned int, unsigned int);
+	template void Image<ImageFormat::HLS, float>::create(unsigned int, unsigned int);
+	template void Image<ImageFormat::HSV, uint8_t>::create(unsigned int, unsigned int);
+	template void Image<ImageFormat::HSV, int16_t>::create(unsigned int, unsigned int);
+	template void Image<ImageFormat::HSV, float>::create(unsigned int, unsigned int);
+	template void Image<ImageFormat::YCrCb, uint8_t>::create(unsigned int, unsigned int);
+	template void Image<ImageFormat::YCrCb, int16_t>::create(unsigned int, unsigned int);
+	template void Image<ImageFormat::YCrCb, float>::create(unsigned int, unsigned int);
+	// ------------------------------------------------------------------------------------------------------------
+	template Pixel<ImageFormat::GRAY, uint8_t> Image<ImageFormat::GRAY, uint8_t>::GetPixel(int, int, const BorderMode<ImageFormat::GRAY, uint8_t>&) const;
+	template Pixel<ImageFormat::GRAY, int16_t> Image<ImageFormat::GRAY, int16_t>::GetPixel(int, int, const BorderMode<ImageFormat::GRAY, int16_t>&) const;
+	template Pixel<ImageFormat::GRAY, float> Image<ImageFormat::GRAY, float>::GetPixel(int, int, const BorderMode<ImageFormat::GRAY, float>&) const;
+	template Pixel<ImageFormat::RGB, uint8_t> Image<ImageFormat::RGB, uint8_t>::GetPixel(int, int, const BorderMode<ImageFormat::RGB, uint8_t>&) const;
+	template Pixel<ImageFormat::RGB, int16_t> Image<ImageFormat::RGB, int16_t>::GetPixel(int, int, const BorderMode<ImageFormat::RGB, int16_t>&) const;
+	template Pixel<ImageFormat::RGB, float> Image<ImageFormat::RGB, float>::GetPixel(int, int, const BorderMode<ImageFormat::RGB, float>&) const;
+
+	// -------------------------------------------------------------------------------------------------------------
+}
