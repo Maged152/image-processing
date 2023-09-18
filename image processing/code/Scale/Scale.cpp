fc5b026b9083a221af280fc500eac61c577b432c
Ahmed Maged <mageda9000@gmail.com>
2023-09-18 21:55:08 +0300

    update scale function

--- image processing/code/Scale/Scale.cpp
+++ image processing/code/Scale/Scale.cpp
@@ -4,7 +4,9 @@
 #include <algorithm>
 
 template<qlm::ImageFormat frmt, qlm::pixel_t T>
-qlm::Image<frmt, T> qlm::Scale(const qlm::Image<frmt, T>& in, qlm::InterpolationFlag method, float scale_x, float scale_y)
+qlm::Image<frmt, T> qlm::Scale(const qlm::Image<frmt, T>& in,
+						       const qlm::InterpolationFlag inter, const float scale_x, const float scale_y,
+							   const BorderMode<frmt, T>& border_mode)
 {
 	unsigned int width = in.Width();
 	unsigned int height = in.Height();
@@ -13,131 +15,42 @@ qlm::Image<frmt, T> qlm::Scale(const qlm::Image<frmt, T>& in, qlm::Interpolation
 	unsigned int new_height = height * scale_y;
 
 	qlm::Image<frmt, T> out;
-	out.create(new_width, new_height, qlm::Pixel<frmt, T>{});
-	// check the scaling method
-	if (method == qlm::InterpolationFlag::NEAREST_NEIGHBOR)
+	out.create(new_width, new_height, border_mode.border_pixel);
+
+	for (int x = 0; x < new_width; x++)
 	{
-		qlm::Pixel<frmt, T> c;
-		for (int x = 0; x < new_width; x++)
+		for (int y = 0; y < new_height; y++)
 		{
-			for (int y = 0; y < new_height; y++)
-			{
-				// set the pixel with the nearest neighbor
-				unsigned int nearest_x = std::clamp((unsigned int)std::round(x / scale_x), 0u, width - 1);
-				unsigned int nearest_y = std::clamp((unsigned int)std::round(y / scale_y), 0u, height - 1);
-				c = in.GetPixel(nearest_x, nearest_y);
-				out.SetPixel(x, y, c);
-			}
+			// value from input image
+			float in_x = x / scale_x;
+			float in_y = y / scale_y;
+			// check if inside the image
+			if (in_x < 0 || in_y < 0 || in_x >= in.Width() || in_y >= in.Height())
+				continue;
+			// do interpolation
+			const auto pix = qlm::Interpolation(in, in_x, in_y, inter, border_mode);
+			// store pixel
+			out.SetPixel(x, y, pix);
+
 		}
 	}
-	else if (method == qlm::InterpolationFlag::BILINEAR)
-	{
-		for (int x = 0; x < new_width; x++)
-		{
-			for (int y = 0; y < new_height; y++)
-			{
-			// get the pixel with the nearest neighbor
-			float nearest_x = x / scale_x;
-			float nearest_y = y / scale_y;
-			// get the four points
-			unsigned int x1 = std::floor(nearest_x);
-			unsigned int y1 = std::floor(nearest_y);
-			unsigned int x2 = std::min(x1 + 1, width - 1);
-			unsigned int y2 = std::min(y1 + 1, height - 1);
-			// get the four neighbors
-			qlm::Pixel<frmt, T> top_left = in.GetPixel(x1, y1);
-			qlm::Pixel<frmt, T> top_right = in.GetPixel(x2, y1);
-			qlm::Pixel<frmt, T> bottom_left = in.GetPixel(x1, y2);
-			qlm::Pixel<frmt, T> bottom_right = in.GetPixel(x2, y2);
-			// offsets
-			float x_offset = nearest_x - x1;
-			float y_offset = nearest_y - y1;
-			// interpolate along x-axis
-			qlm::Pixel<frmt, T> top = top_left * (1.0f - x_offset) + top_right * x_offset;
-			qlm::Pixel<frmt, T> bottom = bottom_left * (1.0f - x_offset) + bottom_right * x_offset;
-			// interpolate along y-axis
-			qlm::Pixel<frmt, T> out_pixel = top * (1.0f - y_offset) + bottom * y_offset;
-			out.SetPixel(x, y, out_pixel);
-			}
-		}
-	}
-	else
-	{
-		for (int x = 0; x < new_width; x++)
-		{
-			for (int y = 0; y < new_height; y++)
-			{
-				// get the pixel with the nearest neighbor
-				float nearest_x = x / scale_x;
-				float nearest_y = y / scale_y;
-				//
-				unsigned int x1 = std::floor(nearest_x);
-				unsigned int y1 = std::floor(nearest_y);
-				// offsets
-				float xoff = nearest_x - x1;
-				float yoff = nearest_y - y1;
-				float xxoff{ xoff * xoff }, xxxoff{ xoff * xoff * xoff };
-				float yyoff{ yoff * yoff }, yyyoff{ yoff * yoff * yoff };
 
-				// 16 point to sample
-				qlm::Pixel<frmt,float> bicubic_points[4][4]{};
-				for (int i = 0; i < 4; i++)
-				{
-					for (int j = 0; j < 4; j++)
-					{
-						int px = std::clamp(x1 - 1 + i, 0u, width - 1);
-						int py = std::clamp(y1 - 1 + j, 0u, height - 1);
-						bicubic_points[i][j] = in.GetPixel(px, py);
-					}
-				}
-				// x offsets
-				float Q1_x = 0.5f * (-xxxoff + 2.0f * xxoff - xoff);
-				float Q2_x = 0.5f * (3.0f * xxxoff - 5.0f * xxoff + 2.0f);
-				float Q3_x = 0.5f * (-3.0f * xxxoff + 4.0f * xxoff + xoff);
-				float Q4_x = 0.5f * (xxxoff - xxoff);
-				// y offsets
-				float Q1_y = 0.5f * (-yyyoff + 2.0f * yyoff - yoff);
-				float Q2_y = 0.5f * (3.0f * yyyoff - 5.0f * yyoff + 2.0f);
-				float Q3_y = 0.5f * (-3.0f * yyyoff + 4.0f * yyoff + yoff);
-				float Q4_y = 0.5f * (yyyoff - yyoff);
-
-				qlm::Pixel<frmt, float> bicubic_points_y[4]{};
-				for (int i = 0; i < 4; i++)
-				{
-					bicubic_points_y[i] = bicubic_points[i][0] * Q1_x + bicubic_points[i][1] * Q2_x + bicubic_points[i][2] * Q3_x + bicubic_points[i][3] * Q4_x;
-				}
-
-				qlm::Pixel<frmt, T> out_pixel;
-				out_pixel = bicubic_points_y[0] * Q1_y + bicubic_points_y[1] * Q2_y + bicubic_points_y[2] * Q3_y + bicubic_points_y[3] * Q4_y;
-
-				out.SetPixel(x, y, out_pixel);
-			}
-		}
-	}
 	return out;
 }
 
 // Explicit instantiation for RGB , uint8_t
 template qlm::Image<qlm::ImageFormat::RGB, uint8_t> 
 qlm::Scale<qlm::ImageFormat::RGB, uint8_t>(const qlm::Image<qlm::ImageFormat::RGB, uint8_t>&,
-										   qlm::InterpolationFlag,
-										   float,
-										   float);
-// Explicit instantiation for RGB , int16_t
-template qlm::Image<qlm::ImageFormat::RGB, int16_t>
-qlm::Scale<qlm::ImageFormat::RGB, int16_t>(const qlm::Image<qlm::ImageFormat::RGB, int16_t>&,
-	qlm::InterpolationFlag,
-	float,
-	float);
+										   const qlm::InterpolationFlag,
+										   const float,
+										   const float,
+										   const BorderMode<qlm::ImageFormat::RGB, uint8_t>&);
+
 // Explicit instantiation for GRAY , uint8_t
 template qlm::Image<qlm::ImageFormat::GRAY, uint8_t>
 qlm::Scale<qlm::ImageFormat::GRAY, uint8_t>(const qlm::Image<qlm::ImageFormat::GRAY, uint8_t>&,
-	qlm::InterpolationFlag,
-	float,
-	float);
-// Explicit instantiation for GRAY , int16_t
-template qlm::Image<qlm::ImageFormat::GRAY, int16_t>
-qlm::Scale<qlm::ImageFormat::GRAY, int16_t>(const qlm::Image<qlm::ImageFormat::GRAY, int16_t>&,
-	qlm::InterpolationFlag,
-	float,
-	float);
+											const qlm::InterpolationFlag,
+											const float,
+											const float,
+											const BorderMode<qlm::ImageFormat::GRAY, uint8_t>&);
+
--- image processing/include/Scale/Scale.h
+++ image processing/include/Scale/Scale.h
@@ -6,7 +6,8 @@ namespace qlm
 {
 	template<ImageFormat frmt, pixel_t T>
 	Image<frmt, T> Scale(const Image<frmt, T>& in,
-							   InterpolationFlag method,
-		                       float scale_x, 
-		                       float scale_y);
-} // namespace
+						 const InterpolationFlag method,
+		                 const float scale_x, 
+		                 const float scale_y,
+						 const BorderMode<frmt, T>& border_mode = BorderMode<frmt, T>{});
+} 
