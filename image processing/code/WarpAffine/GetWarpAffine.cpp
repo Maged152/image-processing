a61b02314ab027b50746158e72311e05d7cf629c
Ahmed Maged <mageda9000@gmail.com>
2023-09-19 23:03:29 +0300

    start implementing gaussian elimination

--- image processing/code/WarpAffine/GetWarpAffine.cpp
+++ image processing/code/WarpAffine/GetWarpAffine.cpp
@@ -32,6 +32,46 @@ namespace qlm
 	{
 		TransformationMatrix out;
 
+		// Gaussian elimination
+		float aug_mat_x[3][4]{};
+		float aug_mat_y[3][4]{};
+		// construct the augmented matrix with the input points
+		/*
+			x_n = m00 * x + m01 * y + m02
+			B = AX
+			|x y 1 | x_n|
+			| ......... |
+			| ......... |
+
+			the same for y
+			y_n = m10 * x + m11 * y + m12
+			|x y 1 | y_n|
+			| ......... |
+			| ......... |
+		*/
+		for (int i = 0; i < 3; i++)
+		{
+			aug_mat_x[i][0] = src[i].x;
+			aug_mat_x[i][1] = src[i].y;
+			aug_mat_x[i][2] = 1.0f;
+			aug_mat_x[i][3] = dst[i].x;
+
+			aug_mat_y[i][0] = src[i].x;
+			aug_mat_y[i][1] = src[i].y;
+			aug_mat_y[i][2] = 1.0f;
+			aug_mat_y[i][3] = dst[i].y;
+		}
+		
+		// function to swap two rows
+		auto SwapRows = [](float** matrix, int row1, int row2)
+		{
+			float* temp = matrix[row1];
+			matrix[row1] = matrix[row2];
+			matrix[row2] = temp;
+			temp = nullptr;
+		};
+		
+		// function to find the pivot
 
 		return out;
 	}
