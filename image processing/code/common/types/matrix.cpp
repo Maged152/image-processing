4473c6879cfc7a8e8ef0ac151d72424c7231ca4a
Ahmed Maged <mageda9000@gmail.com>
2023-11-17 18:05:44 +0200

    add matrix data type

---
+++ image processing/code/common/types/matrix.cpp
@@ -0,0 +1,41 @@
+#include "common/types/matrix.h"
+#include <iostream>
+#include <random>
+#include <iomanip>
+#include <cmath>
+
+namespace qlm
+{
+    void Matrix::Print(int digits) const
+    {
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < columns; c++)
+            {
+                int print_digits = digits;
+                float element = this->Get(r, c);
+
+                if (element != 0)
+                {
+                    int fixed_digits = static_cast<int>(std::log10(std::abs(element))) + 1;
+                    print_digits = fixed_digits >= digits ? 0 : fixed_digits - digits;
+                }
+
+                std::cout << std::fixed << std::setprecision(print_digits) << element << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    void Matrix::RandomInit(const float min_value, const float max_value)
+    {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<float> dis(min_value, max_value);
+
+        for (int i = 0; i < columns * rows; i++)
+        {
+            this->Set(i, dis(gen));
+        }
+    }
+}
---
+++ image processing/include/common/types/matrix.h
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <limits>
+
+
+namespace qlm
+{
+    class Matrix 
+    {
+    private:
+        float* data;
+        int columns;
+        int rows;
+
+
+    public:
+        // Default constructor
+        Matrix() : data(nullptr), columns(0), rows(0)
+        {}
+        // Parameterized constructor
+        Matrix(int r, int c) : columns(c), rows(r)
+        {
+            data = new float[columns * rows];
+        }
+        // Copy constructor
+        Matrix(const Matrix& other) : columns(other.columns), rows(other.rows)
+        {
+            data = new float[columns * rows];
+            for (int i = 0; i < columns * rows; ++i) {
+                data[i] = other.data[i];
+            }
+        }
+        // Destructor
+        ~Matrix()
+        {
+            rows = columns = 0;
+            if (data != nullptr)
+                delete[] data;
+        }
+        // move and = operator
+    public:
+        // Setter for individual element
+        void Set(int row, int col, float value)
+        {
+            if (row >= 0 && row < rows && col >= 0 && col < columns)
+            {
+                data[row * columns + col] = value;
+            }
+        }
+        void Set(int i, float value) {
+            if (i >= 0 && i < columns * rows)
+            {
+                data[i] = value;
+            }
+        }
+        // Getter for individual element
+        float Get(int row, int col) const
+        {
+            if (row >= 0 && row < rows && col >= 0 && col < columns)
+            {
+                return data[row * columns + col];
+            }
+            return std::numeric_limits<float>::signaling_NaN();
+        }
+        float Get(int i) const
+        {
+            if (i >= 0 && i < columns * rows)
+            {
+                return data[i];
+            }
+            return std::numeric_limits<float>::signaling_NaN();
+        }
+        // Getter for columns
+        int Columns() const
+        {
+            return columns;
+        }
+        // Getter for rows
+        int Rows() const
+        {
+            return rows;
+        }
+    public:
+        // print matrix
+        void Print(int digits = 5) const;
+        // random initialization
+        void RandomInit(const float min_value, const float max_value);
+    };
+}
