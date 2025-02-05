497b064bd303bce6480d9202d192f90fc697c6f8
Ahmed Maged <mageda9000@gmail.com>
2025-02-05 20:54:32 +0200

    add PixelImage

--- cmake/FetchDependencies.cmake
+++ cmake/FetchDependencies.cmake
@@ -11,3 +11,15 @@ set(ThreadPool_BUILD_EXAMPLES OFF CACHE BOOL "Disable examples" FORCE)
 set(ThreadPool_BUILD_Doc OFF CACHE BOOL "Disable doc" FORCE)
 
 FetchContent_MakeAvailable(thread_pool)
+
+####################################### Fetch PixelImage #######################################
+FetchContent_Declare(
+  pixel_image
+  GIT_REPOSITORY https://github.com/Maged152/PixelImage.git
+  GIT_TAG c67a60e4ad49868ddd35361cfe1fd6fe08f53ccf #v1.0.1 
+
+)
+
+set(PixelImage_BUILD_EXAMPLES OFF CACHE BOOL "Disable examples" FORCE)
+
+FetchContent_MakeAvailable(pixel_image)
--- include/common/pixel.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "common/pixel/pixel_GRAY.hpp"
-#include "common/pixel/pixel_HLS.hpp"
-#include "common/pixel/pixel_HSV.hpp"
-#include "common/pixel/pixel_RGB.hpp"
-#include "common/pixel/pixel_YCrCb.hpp"
-
-
