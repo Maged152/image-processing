34ebd98ee6f3e2038fc1c73cda6517a0942b4311
Ahmed Maged <mageda9000@gmail.com>
2026-06-24 21:29:26 +0300

    fix possible overflow

--- source/Magnitude.cpp
+++ source/Magnitude.cpp
@@ -1,11 +1,13 @@
 #include "Magnitude.hpp"
+#include <algorithm>
 
 template <qlm::pixel_t T>
 qlm::Image<qlm::ImageFormat::GRAY, std::make_unsigned_t<T>> qlm::Magnitude(const qlm::Image<qlm::ImageFormat::GRAY, T> &g_x, 
                                                                            const qlm::Image<qlm::ImageFormat::GRAY, T> &g_y, 
                                                                            const bool L2gradient)
 {
-    qlm::Image<qlm::ImageFormat::GRAY, std::make_unsigned_t<T>> out(g_x.width, g_x.height);
+    using res_t = std::make_unsigned_t<T>;
+    qlm::Image<qlm::ImageFormat::GRAY, res_t> out(g_x.width, g_x.height);
 
     for (int y = 0; y < g_x.height; y++)
     {
@@ -14,14 +16,17 @@ qlm::Image<qlm::ImageFormat::GRAY, std::make_unsigned_t<T>> qlm::Magnitude(const
             const double pix_x = static_cast<double>(g_x.GetPixel(x, y).v);
             const double pix_y = static_cast<double>(g_y.GetPixel(x, y).v);
 
-            std::make_unsigned_t<T> magnitude;
+            res_t magnitude;
             if (L2gradient)
             {
-                magnitude = static_cast<std::make_unsigned_t<T>>(std::sqrt(pix_x * pix_x + pix_y * pix_y));
+                magnitude = static_cast<res_t>(std::sqrt(pix_x * pix_x + pix_y * pix_y));
             }
             else
             {
-                magnitude = static_cast<std::make_unsigned_t<T>>(std::abs(pix_x) + std::abs(pix_y));
+                magnitude = static_cast<res_t>(std::clamp(std::abs(pix_x) + std::abs(pix_y), 
+                                                          static_cast<double>(std::numeric_limits<res_t>::min()), 
+                                                          static_cast<double>(std::numeric_limits<res_t>::max())
+                                                         ));
             }
 
             out.SetPixel(x, y, magnitude);
