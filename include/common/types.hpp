5c95f5938224f133c04e267305a6ce2e883b9ecf
Ahmed Maged <mageda9000@gmail.com>
2026-07-28 01:14:37 +0300

    update kp

--- include/common/types.hpp
+++ include/common/types.hpp
@@ -28,6 +28,11 @@ namespace qlm
 		float angle;
 	};
 
+	struct TermCriteria
+	{
+		int max_count = 0;
+		double epsilon = 0.0;
+	};
 
 	template<class T = int>
 	class Point
@@ -86,18 +91,20 @@ namespace qlm
 	};
 
 	template<class T = int>
-	struct KeyPoint {
-		// Member variables
+	struct KeyPoint 
+	{
 		Point<T> point;
 		float scale;
 		float angle;
 		float response;
+		float error;
+		int status;
 		int level;
 		int class_id;
 
 		// Constructor
-		KeyPoint(Point<T> _pt, float _response = 0, float _scale = 0, float _angle = -1, int _level = 0, int _class_id = -1)
-			: point(_pt), scale(_scale), angle(_angle), response(_response), level(_level), class_id(_class_id)
+		KeyPoint(Point<T> _pt, float _response = 0, float _scale = 0, float _angle = -1, int _level = 0, int _class_id = -1, float _error = -1, int _status = -1)
+			: point(_pt), scale(_scale), angle(_angle), response(_response), error(_error), status(_status), level(_level), class_id(_class_id)
 		{}
 	};
 
